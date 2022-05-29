05335b04945eab10857c46c53dfe6018c8b8fb7a
Md Iqbal Hossain <iqbalhossainnbd@gmail.com>
2022-05-29 06:15:28 +0600

    covid-19 react-native

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import {colors} from "./src/theme/colors";
+import {spacing} from "./src/theme/spacing";
+import {useFonts} from 'expo-font';
+import {typography} from './src/theme/typography';
+import Text from './src/components/text/text';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import home from './src/screens/home';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require ('./assets/fonts/Antonio-Medium.ttf'),
+    'League-Spartan-Regular' : require ('./assets/fonts/LeagueSpartan-Regular.ttf'),
+    'League-Spartan-Bold' : require ('./assets/fonts/LeagueSpartan-Bold.ttf'),
+  });
+  
+  if (!loaded) {
+    return <Text>Font is loading...</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.orange,
     alignItems: 'center',
     justifyContent: 'center',
   },
